4053369afede3de13f6d4b01486a99955602de79
Xwilarg <xwilarg@protonmail.com>
2026-08-15 19:53:13 +0200

    Update targets

---
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OneHourGameJam.Manager
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
